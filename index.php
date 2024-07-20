9ebf4d2d55e485d40786a138eefd18766721b7f4
Imran Shah <37284083+imran786571@users.noreply.github.com>
2024-07-20 11:18:17 -0700

    Add files via upload
    display the name on the browser

---
+++ index.php
@@ -0,0 +1,12 @@
+
+
+<?php
+
+// echo is a language constract to dispaly about on browser
+
+// Now to display the name on browser
+echo "Hello World <br />";
+echo "My Name is: Imran Shah";
+
+
+?>
